be9f235afdedca32fea2eac188d220d191dba985
joshrizzo <josh.rizzo@gmail.com>
2015-02-15 21:05:09 -0600

    Initial Commit

---
+++ DomParserTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DomParserTest {
+
+	@Test
+	public void test() {
+		DomParserExample dpe = new DomParserExample();
+		dpe.runExample();
+	}
+}
